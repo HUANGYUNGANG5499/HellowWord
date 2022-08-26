4857b71235246232fb8a4b848e49c749405170a2
HUANGYUNGANG5499 <y1083816082@gmail.com>
2022-08-27 00:11:23 +0900

    TEST

---
+++ TestIfElse3.java
@@ -0,0 +1,16 @@
+public class TestIfElse3 {
+	public static void main(String[] args) {
+		int x =40;
+		if(x==10) {
+			System.out.println("x的数值是10");
+		} else if (x == 20) {
+			System.out.println("x的数值是20");
+		} else if (x == 30) {
+			System.out.println("x的数值是30");
+		} else {
+			System.out.println("这厮else语句");
+		}
+	}
+}
+			
+		
